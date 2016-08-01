f175c163e8ece1e984ee48d0d0750be29f767cdf
rnelson <eng.russell.nelson@gmail.com>
2016-07-31 22:24:29 -0500

    stuff

---
+++ lib/main_app.dart
@@ -0,0 +1,52 @@
+// Copyright (c) 2016, rnelson. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+@HtmlImport('main_app.html')
+library untitled1.lib.main_app;
+
+import 'dart:html';
+
+import 'package:polymer_elements/paper_input.dart';
+import 'package:polymer_elements/paper_button.dart';
+import 'package:polymer_elements/paper_material.dart';
+import 'package:polymer_elements/paper_scroll_header_panel.dart';
+import 'package:polymer_elements/paper_toolbar.dart';
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart';
+
+/// Uses [PaperButton, PaperInput, PaperMaterial, PaperScrollHeaderPanel, PaperToolbar, CodeBlock]
+@PolymerRegister('main-app')
+class MainApp extends PolymerElement {
+  @property
+  String text;
+
+  /// Constructor used to create instance of MainApp.
+  MainApp.created() : super.created();
+
+  @reflectable
+  String reverseText(String text) {
+    return text.split('').reversed.join('');
+  }
+
+  // Optional lifecycle methods - uncomment if needed.
+
+//  /// Called when an instance of main-app is inserted into the DOM.
+//  attached() {
+//    super.attached();
+//  }
+
+//  /// Called when an instance of main-app is removed from the DOM.
+//  detached() {
+//    super.detached();
+//  }
+
+//  /// Called when an attribute (such as a class) of an instance of
+//  /// main-app is added, changed, or removed.
+//  attributeChanged(String name, String oldValue, String newValue) {
+//    super.attributeChanged(name, oldValue, newValue);
+//  }
+
+//  /// Called when main-app has been fully prepared (Shadow DOM created,
+//  /// property observers set up, event listeners attached).
+//  ready() {
+//  }
+}
